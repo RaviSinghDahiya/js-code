f15c2bf64e50ac43f7bc8c5e4081ff647863c967
RaviSinghDahiya <108272855+RaviSinghDahiya@users.noreply.github.com>
2024-06-21 16:34:50 +0000

    datatypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,17 @@
+
+let name = "Ravi";
+let age = 27;
+let city = "Hansi";
+let isLoggedIn = false
+
+// number => 2 to power 53 
+// string => ""
+// bigInt => for long numbers
+// boolean => true or false
+// null => standalone value 
+// undefined => 
+// symbol => unique ke liye 
+// object => 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
