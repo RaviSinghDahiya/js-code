ed8c133d6080e711af37a6bd7f1e2b2ec2bc91aa
RaviSinghDahiya <108272855+RaviSinghDahiya@users.noreply.github.com>
2024-06-23 06:01:17 +0000

    data summary

---
+++ data-summary.js
@@ -0,0 +1,25 @@
+// Primitive
+// string, number, boolean, null, undefined, bigInt, symobol, 
+let inString = "Ravi";
+let inNumber = 23;
+let isLoggedIn = false;
+let outsideTemp = null;
+let userEmail = undefined;
+let largeNumber = 23788253253295n;
+
+const ID = Symbol("123");
+const accoundID = Symbol("123");
+
+// Non Primitive
+// Array, Object, Function
+let arr = ["Abc", "def", "hij"];
+let obj = {name: "Ravi", age: 27, city: "Noida"}
+
+let fun = function(){
+    console.log('hello world');
+}
+
+console.log(fun());
+
+
+
